3c404234129749736961cd0a7660cb536d983376
KIM Taegyoon <steloflute@gmail.com>
2018-07-05 00:29:05 +0900

    fixed protocol(HTTPS) and regex

--- naver-top.cpp
+++ naver-top.cpp
@@ -34,21 +34,21 @@ string get_text() {
 	// Specify an HTTP server.
 	if (hSession)
 		hConnect = WinHttpConnect(hSession, L"www.naver.com",
-		INTERNET_DEFAULT_HTTP_PORT, 0);
+			INTERNET_DEFAULT_HTTPS_PORT, 0);
 
 	// Create an HTTP request handle.
 	if (hConnect)
 		hRequest = WinHttpOpenRequest(hConnect, L"GET", NULL,
-		NULL, WINHTTP_NO_REFERER,
-		WINHTTP_DEFAULT_ACCEPT_TYPES,
-		0);
+			NULL, WINHTTP_NO_REFERER,
+			WINHTTP_DEFAULT_ACCEPT_TYPES,
+			WINHTTP_FLAG_SECURE);
 
 	// Send a request.
 	if (hRequest)
 		bResults = WinHttpSendRequest(hRequest,
-		WINHTTP_NO_ADDITIONAL_HEADERS, 0,
-		WINHTTP_NO_REQUEST_DATA, 0,
-		0, 0);
+			WINHTTP_NO_ADDITIONAL_HEADERS, 0,
+			WINHTTP_NO_REQUEST_DATA, 0,
+			0, 0);
 
 
 	// End the request.
@@ -64,7 +64,7 @@ string get_text() {
 			dwSize = 0;
 			if (!WinHttpQueryDataAvailable(hRequest, &dwSize))
 				printf("Error %u in WinHttpQueryDataAvailable.\n",
-				GetLastError());
+					GetLastError());
 
 			// Allocate space for the buffer.
 			pszOutBuffer = new char[dwSize + 1];
@@ -122,15 +122,17 @@ string UTF8ToANSI(string s)
 vector<string> list_naver() {
 	string s(UTF8ToANSI(get_text()));
 
-	static regex e("<option value=\".+\">.+: (.+)</option>");
+	static regex e("<span class=\"ah_k\">(.+?)</span>");
 	regex_iterator<string::iterator> rit(s.begin(), s.end(), e);
 	static regex_iterator<string::iterator> rend;
 
 	vector<string> r;
 
-	while (rit != rend) {
+	size_t count = 0;
+	while (rit != rend && count < 20) {
 		r.push_back((*rit)[1]);
 		++rit;
+		count++;
 	}
 	return r;
 }
@@ -159,7 +161,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		cout << endl << endl;
 		Sleep(60 * 1000);
 	}
-	
+
 	return 0;
 }
 
